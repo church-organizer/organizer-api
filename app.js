03f21bcc34f34662b7787e6c007bea0d4563c282
nloetkemann <n.loetkemann@mittwald.de>
2019-08-28 16:39:00 +0200

    dockerfile with allow all

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // set header
 app.use(function(req, res, next) {
     res.header("Content-Type", "application/json");
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
