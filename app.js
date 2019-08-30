0b018cb3627c53ab0b78d95e77115f630f234865
nloetkemann <n.loetkemann@mittwald.de>
2019-08-30 08:37:10 +0200

    more infos

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // set header
 app.use(function(req, res, next) {
     res.header("Content-Type", "application/json");
+    console.log(process.env.WIKI_HOST);
     if (process.env.WIKI_HOST !== undefined) {
-        console.log("Wiki-Host exitst");
+
         res.header("Access-Control-Allow-Origin", [process.env.WIKI_HOST, "http://localhost:3000"]);
     } else {
         res.header("Access-Control-Allow-Origin", "http://localhost:3000");
